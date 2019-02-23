d6e54dd2956d9c7cf4524ddb754e666f254d76f2
Khaireddine <maksystem10@gmail.com>
2019-02-23 12:41:57 +0100

    first upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import Header from 'header';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        version : 2019,
+        label : "Mak System",
+        urlOfSite : "https://www.google.com"
+    }
+    render() {
+        return (
+            <div className="App">
+                <Header name={this.state.version} label={this.state.label} version={this.state.version}/>
+                <MainContent/>
+                <Footer/>
+            </div>
+        );
+    }
 }
+
+
+class Header extends Component {
+    state = {
+        siteUrl: "https://www.google.com"
+    }
+
+    render() {
+        return (
+            <div className="header">
+                <h2>
+                    <a href={this.state.siteUrl} target="_blank">{this.props.label} </a> {this.props.version}
+                </h2>
+            </div>
+        )
+    }
+}
+
+class MainContent extends React.Component {
+    state = {
+        version: 2019
+    }
+
+    render() {
+        return (
+            <main className="main">
+                <h3>
+                    This is Main component for this site {this.state.version}
+                </h3>
+                <LeftContent/>
+                <RightContent/>
+
+            </main>
+        )
+    }
+}
+
+class LeftContent extends React.Component {
+    render() {
+        return (
+            <aside className="col-left">
+                <h3>
+                    Left aside
+                </h3>
+            </aside>
+        )
+    }
+}
+
+class RightContent extends React.Component {
+    render() {
+        return (
+            <aside className="col-left">
+                <h3>
+                    Right aside
+                </h3>
+            </aside>
+        )
+    }
+}
+
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                This footer for our site
+            </footer>
+        )
+    }
+}
+
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h2>
+                    This is header component
+                </h2>
+            </div>
+        )
+    }
+}
+
+export default Header;
